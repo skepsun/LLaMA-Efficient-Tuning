50bd0934a9ee4eddcf10cea6f9da0578994ad8e0
skepsun <chuxiongsun@gmail.com>
2023-09-07 15:44:43 +0800

    Update general_args.py

--- src/llmtuner/hparams/general_args.py
+++ src/llmtuner/hparams/general_args.py
@@ -7,7 +7,7 @@ class GeneralArguments:
     r"""
     Arguments pertaining to which stage we are going to perform.
     """
-    stage: Optional[Literal["pt", "sft", "rm", "ppo", "dpo"]] = field(
+    stage: Optional[Literal["pt", "sft", "rm", "rs", "ppo", "dpo"]] = field(
         default="sft",
         metadata={"help": "Which stage will be performed in training."}
     )

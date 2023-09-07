c7ac80d95f54d668f4b1515d72033970528b7375
skepsun <chuxiongsun@gmail.com>
2023-09-07 15:45:19 +0800

    Update generating_args.py

--- src/llmtuner/hparams/generating_args.py
+++ src/llmtuner/hparams/generating_args.py
@@ -27,6 +27,10 @@ class GeneratingArguments:
         default=1,
         metadata={"help": "Number of beams for beam search. 1 means no beam search."}
     )
+    num_return_sequences: Optional[int] = field(
+        default=1,
+        metadata={"help": "The number of sequence candidates to return for each input."}
+    )
     max_length: Optional[int] = field(
         default=None,
         metadata={"help": "The maximum length the generated tokens can have. It can be overridden by max_new_tokens."}
